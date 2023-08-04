61e5cb1ca8793947cd97c9013d2aafcb676a7068
Yanpol Github Cripto <111457843+Tucanoide@users.noreply.github.com>
2023-08-04 09:47:13 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import styles from "../styles/Home.module.css";
 import InstructionsComponent from "../components/InstructionsComponent";
+import TokenBalancesDisplay from "./components/tokensBalanceDisplay.jsx";
 
 export default function Home() {
   return (
